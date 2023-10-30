2e4fa113bb8ca426dd4a2c31eaa4f9856be9979f
Discreater <Discreater@users.noreply.github.com>
2023-10-30 08:39:19 +0000

    deploy: 06bd180a1fcd18d64131c84e007da0a656ce021e

---
+++ assets/10-df7b6f06.js
@@ -0,0 +1,1 @@
+import{aF as s,au as a,av as o,az as e}from"./main-93e0abe8.js";const n={class:"prose prose-sm m-auto text-left"},c=e("h2",{id:"10.20",tabindex:"-1"},"10.20",-1),r=e("p",null,"今天给博客添加了显示日记功能，能写多久写多久。",-1),_=[c,r],u=[],f={__name:"10",setup(d,{expose:t}){return t({frontmatter:{meta:[]}}),s({meta:[]}),(i,h)=>(a(),o("div",n,_))}};export{f as default,u as meta};
