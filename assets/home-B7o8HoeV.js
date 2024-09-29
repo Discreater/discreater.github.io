e976d9b920f21b7941a49ed68a7e90c09db70241
Discreater <Discreater@163.com>
2024-09-29 17:06:06 +0000

    Deploying to gh-pages from @ Discreater/discreater.github.io@ef71274c6e517c120bbf0f56305244767948a440 🚀

---
+++ assets/home-B7o8HoeV.js
@@ -0,0 +1,1 @@
+import{S as o,a}from"./SpotLight-C6CkVOkC.js";import{d as r,r as n,o as s,c as m,e}from"./main-DURqc4wU.js";const c={class:"px-4 py-10 text-center max-w-42rem ml-auto mr-auto text-gray-700 dark:text-gray-200"},x=r({__name:"home",setup(_){return(p,i)=>{const t=n("RouterView");return s(),m("main",c,[e(o),e(t),e(a)])}}});export{x as default};
